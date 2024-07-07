eb222a9bc2afeb366893f2176571157aa031bce3
VelasquezE <132410679+VelasquezE@users.noreply.github.com>
2024-07-07 14:45:09 -0500

    Apply initialization and visualization of the system

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,23 @@
 #include "rendering.h"
 #include "libraries.h"
+#include "physics/systemDynamics.h"
 
 int main(void)
 {
-    /*
-    Constants
-    ----------
-    h (timestep)
-    N (number of particles)
-    */
-
-    /*
-    Initial state vector (initial positions)
-    ------------------------------------------------------
-    std::vector<double> initialState(N);
-        -Each element must be a vector with the data of x, y and z
-    */
-
     initializeGLFW();
 
-    int width = 800;
-    int height = 600;
+    const int N = 100; // Has to be an even value
+    const float particleSeparation = 0.0125;
+    std::vector<Particle> system(N);
+    initializeParticles(N, system, particleSeparation);
 
-    GLFWwindow *window = glfwCreateWindow(width, height, "LearnOpenGL", NULL, NULL);
+    std::vector<glm::vec2> vertices(N);
+    vertexArrayCreation(N, system, vertices);
+
+    float width = 800.0;
+    float height = 800.0;
+
+    GLFWwindow *window = glfwCreateWindow(width, height, "ClothSimulation", NULL, NULL);
     checkWindowCreation(window);
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
@@ -31,18 +26,44 @@ int main(void)
 
     glViewport(0, 0, width, height);
 
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    Shader shaderProgram("../src/shaders/vertexShader.vs",
+                         "../src/shaders/fragmentShader.fs",
+                         "../src/shaders/geometryShader.gs");
+
+    // Create vertex buffer/array object
+    unsigned int VBO;
+    unsigned int VAO;
+    // Generate a buffer ID
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+    // Specify the type of the buffer object
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // Initializes a buffer object's data
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec2), vertices.data(), GL_DYNAMIC_DRAW);
+
+    //  Interpret vertex data
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), (void *)0);
+    glEnableVertexAttribArray(0);
 
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(0.733f, 0.733f, 0.733f, 1.0f);
         processInput(window);
 
         glClear(GL_COLOR_BUFFER_BIT);
+
+        shaderProgram.use();
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_POINTS, 0, vertices.size());
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
+
     return 0;
 }
